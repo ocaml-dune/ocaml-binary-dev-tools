df982f72780e2e8cf5156d3d6fc52d0b7ee8a0b0
Stephen Sherratt <stephen@sherra.tt>
2024-10-15 15:22:21 +1100

    Initial commit

---
+++ ocamlformat/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = {
+          ocamlformat = pkgs.ocamlformat.overrideAttrs {
+            version = "0.25.1";
+            ocaml = "foo";
+          };
+        };
+      });
+}
