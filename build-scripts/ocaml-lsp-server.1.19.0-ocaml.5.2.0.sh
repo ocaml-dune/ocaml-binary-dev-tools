f860fb307a9f711c289790f0dcb39d7ea2785bc6
Stephen Sherratt <stephen@sherra.tt>
2024-11-14 17:25:17 +1100

    Add script to build ocaml-lsp-server (#6)
    Signed-off-by: Stephen Sherratt <stephen@sherra.tt>

---
+++ build-scripts/ocaml-lsp-server.1.19.0-ocaml.5.2.0.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -eu
+
+TAG=$1
+TARGET=$2
+OUTPUT=$3
+
+OCAML_VERSION=5.2.0
+OCAML_LSP_SERVER_VERSION=1.19.0
+
+TMP_DIR="$(mktemp -d -t ocaml-binary-packages-build.XXXXXXXXXX)"
+trap 'rm -rf "$TMP_DIR"' EXIT
+
+cd "$TMP_DIR"
+opam switch create . "ocaml.$OCAML_VERSION"
+eval "$(opam env)"
+opam install -y "ocaml-lsp-server.$OCAML_LSP_SERVER_VERSION"
+
+ARCHIVE_NAME="ocaml-lsp-server.$OCAML_LSP_SERVER_VERSION+binary-ocaml-$OCAML_VERSION-built-$TAG-$TARGET"
+
+mkdir -p "$ARCHIVE_NAME"
+cp _opam/bin/ocamllsp "$ARCHIVE_NAME"
+cat > "$ARCHIVE_NAME/README.md" <<EOF
+# ocaml-lsp-server binary distribution
+
+See https://github.com/ocaml-dune/ocaml-binary-packages for more information.
+EOF
+tar --format=posix -cvf "$ARCHIVE_NAME.tar" "$ARCHIVE_NAME"
+gzip -9 "$ARCHIVE_NAME.tar"
+mv "$ARCHIVE_NAME.tar.gz" $OUTPUT
